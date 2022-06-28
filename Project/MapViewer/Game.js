e0ed168cb582605bdd9635bb57ab40a4af8bb4d7
Jorge Oliveira <jorge.am.oliveira@ua.pt>
2022-06-28 16:46:18 +0100

    Created and added a map to a threejs renderer

---
+++ Project/MapViewer/Game.js
@@ -0,0 +1,262 @@
+import * as THREE from '../libs/three.module.js';
+import { GLTFLoader } from '../libs/GLTFLoader.js';
+import { RGBELoader } from '../libs/RGBELoader.js';
+import { OrbitControls } from '../libs/OrbitControls.js';
+
+class Game{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+        
+		this.clock = new THREE.Clock();
+
+
+		this.assetsPath = '../assets/';
+        
+		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 700 );
+		this.camera.position.set( -100, 100, -150 );
+		// Grass
+		//this.camera.position.set( -1, 1, -1 );
+		// Mountain
+		//this.camera.position.set( 20, 5, 50 );
+		// Sand
+		//this.camera.position.set( 53, 7.65, -61 );
+        
+		let backgroundColor = 0x00ABFF;
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color( backgroundColor );
+		//this.scene.fog = new THREE.Fog( backgroundColor, 100, 200 );
+
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xaaaaff, 0.7);
+		this.scene.add(ambient);
+
+        //Add light with shadow
+		const light = new THREE.DirectionalLight();
+		light.position.set(-40, 120, 100);
+		light.castShadow = true;
+
+		light.shadow.mapSize.width = 2048;
+		light.shadow.mapSize.height = 1024;
+		light.shadow.camera.near = 25;
+		light.shadow.camera.far = 500;
+		light.shadow.bias = 0.0001
+
+		const d = 125;
+		light.shadow.camera.left = -d;
+		light.shadow.camera.bottom = -d;
+		light.shadow.camera.right = light.shadow.camera.top = d;
+
+		this.scene.add(light);
+		this.light = light;
+
+		const helper = new THREE.CameraHelper(light.shadow.camera);
+		this.scene.add(helper);
+
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
+		this.renderer.shadowMap.enabled = true;
+		this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+        //this.renderer.outputEncoding = THREE.sRGBEncoding;
+		container.appendChild( this.renderer.domElement );
+        //this.setEnvironment();
+        
+        const controls = new OrbitControls( this.camera, this.renderer.domElement );
+        
+        this.load();
+		
+		window.addEventListener('resize', this.resize.bind(this) );
+        
+	}
+	
+    resize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+    	this.camera.updateProjectionMatrix();
+    	this.renderer.setSize( window.innerWidth, window.innerHeight ); 
+    }
+    
+    setEnvironment(){
+        const loader = new RGBELoader().setDataType( THREE.UnsignedByteType ).setPath(this.assetsPath);
+        const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
+        pmremGenerator.compileEquirectangularShader();
+        
+        const self = this;
+        
+    }
+    
+	load(){
+        this.loadEnvironment();
+    }
+
+    loadEnvironment(){
+    	const loader = new GLTFLoader( ).setPath(this.assetsPath);
+        
+		// Load GLTF map
+		loader.load(
+			// resource path
+			'mapv4.glb',
+			gltf => {
+				this.scene.add(gltf.scene);
+				this.map = gltf.scene;
+
+				const mergeObjects = {};
+				
+				gltf.scene.traverse(child => {
+					if(child.isMesh){
+						//console.log(child.name);
+						if(child.name.includes("NavMesh")){
+							console.log("Found a navmesh");
+							child.visible = false;
+							this.navmesh = child;
+							// console.log(this.navmesh)
+						}
+						else{
+							child.castShadow = true;
+							child.receiveShadow = true;
+							if(mergeObjects[child.material.name] == undefined){
+								mergeObjects[child.material.name] = [];
+							}
+							mergeObjects[child.material.name].push(child);
+						}
+					}
+				});
+
+				for(let mat in mergeObjects){
+					//console.log(mat + " - " +  mergeObjects[mat].length)
+					const objects = mergeObjects[mat];
+					let material, color;
+					objects.forEach(object => {
+						if(material == undefined){
+							if(mat.includes("Terrain")){
+								const textureLoader = new THREE.TextureLoader();
+								const baseColor = textureLoader
+									.load(this.assetsPath + "textures/grass/Stylized_Grass_003_basecolor.jpg");
+								baseColor.wrapS = baseColor.wrapT = THREE.RepeatWrapping;
+								baseColor.repeat.set(256, 256);
+								baseColor.anisotropy = 16;
+								baseColor.encoding = THREE.sRGBEncoding;
+
+								const ambientOcclusion = textureLoader
+									.load(this.assetsPath + "textures/grass/Stylized_Grass_003_ambientOcclusion.jpg");
+								const height = textureLoader
+									.load(this.assetsPath + "textures/grass/Stylized_Grass_003_height.png");
+								const normal = textureLoader
+									.load(this.assetsPath + "textures/grass/Stylized_Grass_003_normal.jpg");
+								const roughness = textureLoader
+									.load(this.assetsPath + "textures/grass/Stylized_Grass_003_roughness.jpg");
+
+								material = new THREE.MeshLambertMaterial( { 
+									map: baseColor,
+									aoMap: ambientOcclusion
+								});
+								/*
+								material = new THREE.MeshLambertMaterial( { 
+									map: baseColor,
+									normalMap: normal,
+									displacementMap: height,
+									displacementScale: 0.5,
+									roughnessMap: roughness,
+									roughness: 0.5,
+									aoMap: ambientOcclusion
+								});
+								*/
+							}
+							else if(mat.includes("Mountain")){
+								const textureLoader = new THREE.TextureLoader();
+								const baseColor = textureLoader
+									.load(this.assetsPath + "textures/snow/Snow_001_COLOR.jpg");
+								baseColor.wrapS = baseColor.wrapT = THREE.RepeatWrapping;
+								baseColor.repeat.set(128, 128);
+								baseColor.anisotropy = 16;
+								//baseColor.encoding = THREE.sRGBEncoding;
+
+								const ambientOcclusion = textureLoader
+									.load(this.assetsPath + "textures/snow/Snow_001_OCC.jpg");
+								const height = textureLoader
+									.load(this.assetsPath + "textures/snow/Snow_001_DISP.png");
+								const normal = textureLoader
+									.load(this.assetsPath + "textures/snow/Snow_001_NORM.jpg");
+								const roughness = textureLoader
+									.load(this.assetsPath + "textures/snow/Snow_001_ROUGH.jpg");
+
+								material = new THREE.MeshLambertMaterial( { 
+									map: baseColor,
+									aoMap: ambientOcclusion
+								});
+								/*
+								material = new THREE.MeshLambertMaterial( { 
+									map: baseColor,
+									normalMap: normal,
+									displacementMap: height,
+									displacementScale: 0.5,
+									roughnessMap: roughness,
+									roughness: 0.5,
+									aoMap: ambientOcclusion
+								});
+								*/
+							}
+							else if(mat.includes("Orange_3")){
+								const textureLoader = new THREE.TextureLoader();
+								const baseColor = textureLoader
+									.load(this.assetsPath + "textures/sand/Stylized_Sand_001_basecolor.jpg");
+								baseColor.wrapS = baseColor.wrapT = THREE.RepeatWrapping;
+								baseColor.repeat.set(64, 64);
+								baseColor.anisotropy = 16;
+								baseColor.encoding = THREE.sRGBEncoding;
+
+								const ambientOcclusion = textureLoader
+									.load(this.assetsPath + "textures/sand/Stylized_Sand_001_ambientOcclusion.jpg");
+								const height = textureLoader
+									.load(this.assetsPath + "textures/sand/Stylized_Sand_001_height.png.png");
+								const normal = textureLoader
+									.load(this.assetsPath + "textures/sand/Stylized_Sand_001_normal.jpg");
+								const roughness = textureLoader
+									.load(this.assetsPath + "textures/sand/Stylized_Sand_001_roughness.jpg");
+								/*
+								material = new THREE.MeshLambertMaterial( { 
+									map: baseColor,
+									aoMap: ambientOcclusion
+								});
+								*/
+								material = new THREE.MeshStandardMaterial( { 
+									map: baseColor,
+									normalMap: normal,
+									displacementMap: height,
+									displacementScale: 0.5,
+									roughnessMap: roughness,
+									roughness: 0.5,
+									aoMap: ambientOcclusion
+								});
+							}
+							else{
+								//console.log(object.material.map)
+								material = new THREE.MeshPhongMaterial({ 
+									color: object.material.color,
+									shininess: 10
+								});
+							}
+						}
+						object.material = material;
+					})
+				}
+
+				this.renderer.setAnimationLoop(this.render.bind(this));
+			},
+			xhr => {
+
+			},
+			err => {
+				console.error( err );
+			}
+		);
+	}			
+    
+	render() {
+		const dt = this.clock.getDelta();
+
+		//console.log(this.camera.position)
+        this.renderer.render( this.scene, this.camera );
+    }
+}
+
+export { Game };
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ICG_T2
+
+
+Textures:
+
+https://3dtextures.me/2021/03/19/stylized-grass-003/
+https://3dtextures.me/2018/02/26/snow-001/
+
+Assets
+
+https://www.mixamo.com/
+https://assetstore.unity.com/packages/3d/environments/landscapes/lowpoly-environment-pack-99479
+
